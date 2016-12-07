5148432c6162ef3f6e9ba556dd3f793086805b9c
Slavomir Slovenkai <slavomir.slovenkai@student.upjs.sk>
2016-12-07 22:29:13 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/core/db/entity/BankCondition.java
    	src/main/java/core/db/entity/DescriptedBankCondition.java
    	src/main/java/core/db/entity/DescriptedBankConditionCellRenderer.java
    	src/main/java/core/frames/adminBankFrame.java
    	src/main/java/core/frames/adminConditionFrame.java
    	src/main/java/core/frames/adminUserFrame.java
    	src/main/java/core/frames/bankConditionsFrame.java
    	src/main/java/core/frames/bankFrame.java
    	src/main/java/core/frames/bankSearchClientsFrame.java
    	src/main/java/core/frames/bankUpdateBankConditionFrame.java

--- src/main/java/core/db/entity/Condition.java
+++ src/main/java/core/db/entity/Condition.java
@@ -21,11 +21,10 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "myinfocondition")
 public class Condition {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    
     @NotNull
     @Size(min = 1, max = 100)
     @Column(name = "description")
@@ -34,76 +33,60 @@ public class Condition {
     @NotNull
     @Column(name = "expression")
     private String expression;
-
     /**
-     * vracia id podmienky odpovedajuce identifikatoru v tabulke myinfoondition
-     *
+     * vracia id podmienky odpovedajuce identifikatoru v tabulke Condition
      * @return id podmienky
      */
     public Long getId() {
         return id;
     }
-
     /**
-     * nastavuje id podmienky odpovedajuce identifikatoru v tabulke
-     * myinfoondition, AUTOINCREMENT
-     *
+     * nastavuje id podmienky odpovedajuce identifikatoru v tabulke Condition, AUTOINCREMENT
      * @param id id podmienky
      */
     public void setId(Long id) {
         this.id = id;
     }
-
     /**
      * vracia slovny popis podmienky
-     *
      * @return slovny popis podmienky
      */
     public String getDescription() {
         return description;
     }
-
     /**
      * nastavuje slovny popis podmienky, ktory vystihuje podstatu expression
-     *
      * @param description slovny popis podmienky,NotNull,lenght=<1,100>
-     * @see #getExpression()
-     * @see #setExpression(String expression)
+     * @see getExpression()
+     * @see setExpression(String expression)
      */
     public void setDescription(String description) {
         this.description = description;
     }
-
     /**
-     * vracia mysql/sql select ktorý po doplnení parametrov id zakaznika,
-     * požičaná suma a vykonani vráti hodnotu záujmu, ktorá sa na základe Mark
-     * porovná s value a určí sa či je vyskladaná podmienka banky splnená
-     *
-     * @return sql select čakajúci ako parametre id zakaznika , požičaná sumu
+     * vracia vyraz podmienky pomocou ktorej by sa malo vyhodnotit ci uzivatel splna danu podmienku pre danu banku
+     * vyraz v urcitych pripadoch vyuziva value z BankCondition ako parameter
+     * @return vyraz podmienky !!nevie sa ako by mal vizerat!!
      * @see BankCondition
      */
     public String getExpression() {
         return expression;
     }
-
-    /**
-     * nastavuje mysql/sql select ktorý po doplnení parametrov id zakaznika,
-     * požičaná suma a vykonani vráti hodnotu záujmu, ktorá sa na základe Mark
-     * porovná s value a určí sa či je vyskladaná podmienka banky splnená
-     *
-     * @param expression NotNull, dĺžka od 1 do = 50, select čakajúci ako
-     * parametre id zakaznika , požičaná sumu
+/**
+     * nastavuje vyraz podmienky pomocou ktorej by sa malo vyhodnotit ci uzivatel splna danu podmienku pre danu banku
+     * vyraz v urcitych pripadoch vyuziva value z BankCondition ako parameter
+     * @param expression NotNull  !!nevie sa ako by mal vizerat!!
      * @see BankCondition
      */
     public void setExpression(String expression) {
         this.expression = expression;
     }
-    /***
-     * vracia popis podmienky
-     */
-    @Override
-    public String toString() {
-        return description;
-    }
 
+					@Override
+					public String toString()
+					{
+										return description;
+					}
+    
+    
 }
